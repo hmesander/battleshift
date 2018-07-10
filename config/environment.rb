671c250c8bf842882d6b74ea1489a2a9fa8e67ba
Tyler Westlie <tyler.westlie@gmail.com>
2018-07-10 14:57:44 -0600

    added config files

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 '587',
+    domain:               'https://ancient-reaches-55088.herokuapp.com',
+    user_name:            ENV["SENDGRID_USERNAME"],
+    password:             ENV["SENDGRID_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+}
