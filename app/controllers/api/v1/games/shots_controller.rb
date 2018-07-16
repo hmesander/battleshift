2c0e628ab414926e1ddc21c354db42dfad382c85
Haley Mesander <33385692+hmesander@users.noreply.github.com>
2018-07-16 07:45:45 -0600

    Revert "Functionality"

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -2,17 +2,24 @@ module Api
   module V1
     module Games
       class ShotsController < ApiController
-        before_action :player_check
-
         def create
           if wrong_turn?
             render status: 400, json: current_game, message: "Invalid move. It's your opponent's turn."
           elsif wrong_coordinates?
             render status: 400, json: current_game, message: "Invalid coordinates."
           elsif game_over?
-            end_game
+            turn_processor = TurnProcessor.new(current_game, params[:target], current_player, player_1_board, player_2_board)
+            turn_processor.run!
+            if current_game.winner.nil?
+              current_game.update(winner: current_player.email_address)
+              render status: 200, json: current_game, winner: current_game.winner, message: "#{turn_processor.message} Game over."
+            else
+              render status: 200, json: current_game, message: "Invalid move. Game over."
+            end
           else
-            shoot!
+            turn_processor = TurnProcessor.new(current_game, params[:target], current_player, player_1_board, player_2_board)
+            turn_processor.run!
+            render status: turn_processor.status, json: current_game, message: turn_processor.message
           end
         end
 
@@ -27,36 +34,7 @@ module Api
         end
 
         def game_over?
-          current_game.player_1_hits >= 4 || current_game.player_2_hits >= 4
-        end
-
-        def end_game
-          turn_processor = TurnProcessor.new(current_game, params[:target], current_player, player_1_board, player_2_board)
-          turn_processor.run!
-          if current_game.winner.nil?
-            current_game.update(winner: current_player.email_address)
-            render status: 200, json: current_game, winner: current_game.winner, message: "#{turn_processor.message} Game over."
-          else
-            render status: 200, json: current_game, message: "Invalid move. Game over."
-          end
-        end
-
-        def shoot!
-          turn_processor = TurnProcessor.new(current_game, params[:target], current_player, player_1_board, player_2_board)
-          turn_processor.run!
-          render status: turn_processor.status, json: current_game, message: turn_processor.message
-        end
-
-        def player_check
-          render status: 401 unless player_registered? && player_in_game?
-        end
-
-        def player_registered?
-          User.find_by_token(request.headers['X-API-KEY'])
-        end
-
-        def player_in_game?
-          request.headers['X-API-KEY'] == current_game.users[0].token || request.headers['X-API-KEY'] == current_game.users[1].token
+          current_game.player_1_turns >= 4 || current_game.player_2_turns >= 4
         end
       end
     end
--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -3,8 +3,8 @@ FactoryBot.define do
     player_1_board "You should add a board object"
     player_2_board "You should add a board object"
     winner nil
-    player_1_hits 0
-    player_2_hits 0
+    player_1_turns 0
+    player_2_turns 0
     current_turn "challenger"
   end
 end
